d2267e9e4f2a17a76514a48a1df2aae2ceb9b245
Kevin Hill <kevin.hill@fortunabmc.com>
2024-09-30 09:35:03 -0700

    core

--- plugins/core.plugin.js
+++ plugins/core.plugin.js
@@ -1,10 +1,8 @@
-(function (factory) {
-    "function" == typeof define && define.amd ? define(["jquery"], factory) : "object" == typeof module && module.exports ? module.exports = function (e, o) { return void 0 === o && (o = "undefined" != typeof window ? require("jquery") : require("jquery")(e)), factory(o), o; } : factory(jQuery);
-}(function ($) {
-    $.fn.RetoolPlusPlus = function () {
-        console.log("I AM ALIVE")
+(function (window) {
+    window["RetoolPlusPlus"] = function() {
+        console.log("I AM ALIVE");
         return {
             demo: () => console.log("Demooo")
         }
      };
-}));
+})(window);
