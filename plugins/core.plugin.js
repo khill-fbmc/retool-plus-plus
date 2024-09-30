73d6ca0510155252e518662a456215e1f64aa48e
Kevin Hill <kevin.hill@fortunabmc.com>
2024-09-30 09:51:29 -0700

    core

--- plugins/core.plugin.js
+++ plugins/core.plugin.js
@@ -1,8 +1,26 @@
 (function (window) {
-    window["RetoolPlusPlus"] = function() {
-        console.log("I AM ALIVE");
+    let initialized = false;
+
+    /**
+     * Retool Plus Plus
+     *
+     * @param {(...args: any[]) => void} L pretty alias for console.log
+     * @param {(...args: any[]) => void} E pretty alias for console.error
+     * @returns
+     */
+    function RetoolPlusPlus(L, E) {
+        const init = () => {
+            L("Initializing Core Plugin");
+            initialized = true;
+        };
+
+        if (!initialized) init();
         return {
             demo: () => console.log("Demooo")
         }
-     };
+    };
+
+    RetoolPlusPlus.initialized = initialized;
+
+    window["RetoolPlusPlus"] = RetoolPlusPlus;
 })(window);
